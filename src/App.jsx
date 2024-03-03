199dc1e4102c07b7aae0328b38261ad772c53179
Miguel Figueiredo <miguelprimavera76@gmail.com>
2024-03-03 01:01:59 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 // App.js
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Switch } from "react-router";
 import Register from "./Register.jsx";
 import Login from "./Login.jsx";
 
@@ -9,14 +8,12 @@ function App() {
   return (
     <Router>
       <div className="app">
-        <Switch>
           <Route path="/register">
             <Register />
           </Route>
           <Route path="/login">
             <Login />
           </Route>
-        </Switch>
       </div>
     </Router>
   );
